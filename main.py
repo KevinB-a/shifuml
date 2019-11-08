b519bb5fbb00c78aadf22239e1fde273da87e0c7
Kevin <kevin.billet.pro@gmail.com>
2019-11-08 15:44:22 +0100

    improve display

--- main.py
+++ main.py
@@ -19,36 +19,55 @@ while continue_game == True: # create loop for reload the game
 
         if computer_answer == stone and user_answer == paper: # paper win against stone
             score_user+=1
-            print("you win your score is", score_user ,"computer's score is",score_computer) #display both scores
-
+            print("//////////////////////////////////////////////////")
+            print("you win your score is", score_user ) #display user score
+            print("computer's score is",score_computer) #display computer's score
+            print("//////////////////////////////////////////////////")
         if user_answer == stone and computer_answer == scissor: #stone win against scissor
             score_user+=1
-            print("you win your score is", score_user ,"computer's score is",score_computer)
+            print("//////////////////////////////////////////////////")
+            print("you win your score is", score_user ) #display user score
+            print("computer's score is",score_computer) #display computer's score
+            print("//////////////////////////////////////////////////")
 
         if computer_answer == paper and user_answer == scissor: #scissor win against paper
             score_user+=1
-            print("you win your score is",score_user,"computer's score is",score_computer)
-
+            print("//////////////////////////////////////////////////")
+            print("you win your score is", score_user ) #display user score
+            print("computer's score is",score_computer) #display computer's score
+            print("//////////////////////////////////////////////////")
         """computer win case """
 
         if user_answer == stone and computer_answer == paper:
             score_computer+=1
-            print("computer win computer's score is",score_computer,"and your score is",score_user)
+            print("//////////////////////////////////////////////////")
+            print("you win your score is", score_user ) #display user score
+            print("computer's score is",score_computer) #display computer's score
+            print("//////////////////////////////////////////////////")
 
         if user_answer == paper and computer_answer == scissor:
             score_computer+=1
-            print("computer win computer's score is",score_computer,"and your score is ",score_user)
+            print("//////////////////////////////////////////////////")
+            print("you win your score is", score_user ) #display user score
+            print("computer's score is",score_computer) #display computer's score
+            print("//////////////////////////////////////////////////")
 
         if computer_answer == stone and user_answer== scissor:
             score_computer+=1
-            print("computer win computer's score is",score_computer,"and your score is ",score_user)
+            print("//////////////////////////////////////////////////")
+            print("you win your score is", score_user ) #display user score
+            print("computer's score is",score_computer) #display computer's score
+            print("//////////////////////////////////////////////////")
 
         """ draw case"""
         if user_answer == computer_answer:
-            print("draw no one win points your score",score_user,"and computer's score is",score_computer)
-
+            print("//////////////////////////////////////////////////")
+            print("draw no one win ")
+            print("your score",score_user)
+            print("computer's score is",score_computer)
+            print("//////////////////////////////////////////////////")
     rematch=input("do you want to play again ? enter yes for rematch and no for quit").lower()
-    if rematch =="no" or rematch == "n" or rematch =="quit" or rematch == "q":
+    if rematch =="no" or rematch == "n" or rematch =="quit" or rematch == "q" or rematch== "stop":
         continue_game = False #if user choose stop the game continue_game become false and while loop stop
 
     else:
